0b9f046814f295e48a5ae98ed99cb6da6a8bedd3
Ilton Batista <43584705+IltonBJSilva@users.noreply.github.com>
2025-05-10 00:46:38 -0300

    Config pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/IltonBJSilva.github.io",
+  base: '/', // <-- Certo para github.io pessoal
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
-
+})
